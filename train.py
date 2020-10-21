9135eed3d55d4e5469a2ddc4c5f8973888b4e6a3
dterg <dietergalea@gmail.com>
2020-10-21 16:32:17 +0100

    add metrics file

--- train.py
+++ train.py
@@ -2,7 +2,6 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import plot_confusion_matrix
 import matplotlib.pyplot as plt
 import json
-import os
 import numpy as np
 
 # Read in data
@@ -11,17 +10,14 @@ y_train = np.genfromtxt("data/train_labels.csv")
 X_test = np.genfromtxt("data/test_features.csv")
 y_test = np.genfromtxt("data/test_labels.csv")
 
-
 # Fit a model
 depth = 5
 clf = RandomForestClassifier(max_depth=depth)
 clf.fit(X_train,y_train)
 
 acc = clf.score(X_test, y_test)
-print(acc)
-with open("metrics.txt", 'w') as outfile:
-        outfile.write("Accuracy: " + str(acc) + "\n")
-
+with open("metrics.json", 'w') as outfile:
+        json.dump({"accuracy": acc}, outfile)
 
 # Plot it
 disp = plot_confusion_matrix(clf, X_test, y_test, normalize='true',cmap=plt.cm.Blues)
