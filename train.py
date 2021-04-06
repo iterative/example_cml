9062587a1147e7d0b696f9cb2fe66155f1cc4c60
krztfrbn <krztfrbn@gmail.com>
2021-04-06 17:07:48 +0200

    use sample code from cml repo

--- train.py
+++ train.py
@@ -1,29 +1,25 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import plot_confusion_matrix
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-import json
-import os
-import numpy as np
 
-# Read in data
-X_train = np.genfromtxt("data/train_features.csv")
-y_train = np.genfromtxt("data/train_labels.csv")
-X_test = np.genfromtxt("data/test_features.csv")
-y_test = np.genfromtxt("data/test_labels.csv")
 
+# Generate some data
+X,y = make_classification(1000)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=18)
 
 # Fit a model
-depth = 5
+depth = 2
 clf = RandomForestClassifier(max_depth=depth)
 clf.fit(X_train,y_train)
 
+# Assess accuracy on held-out data and print the accuracy
 acc = clf.score(X_test, y_test)
 print(acc)
 with open("metrics.txt", 'w') as outfile:
         outfile.write("Accuracy: " + str(acc) + "\n")
 
-
 # Plot it
 disp = plot_confusion_matrix(clf, X_test, y_test, normalize='true',cmap=plt.cm.Blues)
 plt.savefig('confusion_matrix.png')
-
