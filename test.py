5974209459b635977084f3b837b5af1efad283c9
larrylecomte <>
2021-07-19 21:00:12 -0400

    change to data wine dataset

--- test.py
+++ test.py
@@ -1,28 +1,58 @@
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import plot_confusion_matrix
+import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
-import json
-import os
-import numpy as np
+from sklearn.metrics import plot_confusion_matrix
+import warnings
+warnings.filterwarnings("ignore")
 
 # Read in data
-X_train = np.genfromtxt("data/train_features.csv")
-y_train = np.genfromtxt("data/train_labels.csv")
-X_test = np.genfromtxt("data/test_features.csv")
-y_test = np.genfromtxt("data/test_labels.csv")
+df = pd.read_csv("data/winequality-red-test.csv")
+df
 
+df['quality'].value_counts()
+
+df.info()
+
+df.columns
+
+df.drop(["Id"],axis = 1,inplace = True)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import roc_auc_score
+import statsmodels.api as sm
+from sklearn.metrics import accuracy_score
+
+X = df.drop( "quality",axis=1)
+y = df["quality"]
+
+# use 70% of the data for training and 30% for testing
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=.7, random_state=50)
+
+# RandomForest
+from sklearn.ensemble import RandomForestClassifier
+
+rfc = RandomForestClassifier(n_estimators=50,max_depth=10, random_state=101,class_weight='balanced')
 
 # Fit a model
-depth = 3
-clf = RandomForestClassifier(max_depth=depth)
-clf.fit(X_train,y_train)
+rfc.fit(X_train,y_train)
 
-acc = clf.score(X_test, y_test)
-print(acc)
+y_pred = rfc.predict(X_test)
+
 with open("metrics.txt", 'w') as outfile:
-        outfile.write("Accuracy: " + str(acc) + "\n")
+        outfile.write("F1 Score :" + str(round(f1_score(y_pred,y_test,average = "weighted"), 2)) + "\n")
+        outfile.write("Report:\n" + classification_report(y_test, y_pred) + "\n")
 
 
 # Plot it
-disp = plot_confusion_matrix(clf, X_test, y_test, normalize='true',cmap=plt.cm.Blues)
+disp = plot_confusion_matrix(rfc, X_test, y_test, normalize='true',cmap=plt.cm.Blues)
 plt.savefig('confusion_matrix.png')
+
+# save model file
+with open("model/model.pkcl", 'wb') as pickle_file:
+    pickle.dump(clf, pickle_file)
