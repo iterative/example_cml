3227fb4ec5856483d01a3afa4c78b5b161f9d139
dingyuchen <dingyuchen@gmail.com>
2021-10-23 09:59:33 +0800

    Reformat

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.metrics import plot_confusion_matrix
 import matplotlib.pyplot as plt
 import json
@@ -15,15 +15,16 @@ y_test = np.genfromtxt("data/test_labels.csv")
 # Fit a model
 depth = 2
 clf = RandomForestClassifier(max_depth=depth)
-clf.fit(X_train,y_train)
+# clf = GradientBoostingClassifier(max_depth=depth)
+clf.fit(X_train, y_train)
 
 acc = clf.score(X_test, y_test)
 print(acc)
 with open("metrics.txt", 'w') as outfile:
-        outfile.write("Accuracy: " + str(acc) + "\n")
+    outfile.write("Accuracy: " + str(acc) + "\n")
 
 
 # Plot it
-disp = plot_confusion_matrix(clf, X_test, y_test, normalize='true',cmap=plt.cm.Blues)
+disp = plot_confusion_matrix(
+    clf, X_test, y_test, normalize='true', cmap=plt.cm.Blues)
 plt.savefig('confusion_matrix.png')
-
