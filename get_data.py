2d7752aab340fa2ad08cd29e8f6ed73860356453
Casper da Costa-Luis <casper.dcl@physics.org>
2021-12-30 11:51:27 +0000

    rename plots & tidy (#172)

--- get_data.py
+++ get_data.py
@@ -5,17 +5,15 @@ import os
 
 seed = 42
 # Generate data
-X, y = make_classification(n_samples = 1000, random_state=seed)
+X, y = make_classification(n_samples=1000, random_state=seed)
 
-# Make a train test split
-X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=seed)
+# Make a train/test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=seed)
 
 # Save it
 if not os.path.isdir("data"):
     os.mkdir("data")
-np.savetxt("data/train_features.csv",X_train)
-np.savetxt("data/test_features.csv",X_test)
-np.savetxt("data/train_labels.csv",y_train)
-np.savetxt("data/test_labels.csv",y_test)
-
-
+np.savetxt("data/train_features.csv", X_train)
+np.savetxt("data/test_features.csv", X_test)
+np.savetxt("data/train_labels.csv", y_train)
+np.savetxt("data/test_labels.csv", y_test)
