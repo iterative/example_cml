4c7b2c85221f6876a24d7925fe7f15c508d9b29e
aquilesnubi <aquiles@nubimetrics.com>
2022-02-04 15:59:43 +0000

    New Commit in Integration on 2022-02-04 15:59:43

--- integration.sh
+++ integration.sh
@@ -2,4 +2,4 @@
 
 git add .
 git commit -m "New Commit in Integration on `date +'%Y-%m-%d %H:%M:%S'`"
-git push origin experiment
+git push https://aquilesnubi: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@github.com/aquilesnubi/cml.git origin experiment
