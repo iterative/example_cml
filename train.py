e0f9e77c9b94a700ab1ef06feb6882a8b82700b3
Casper da Costa-Luis <casper.dcl@physics.org>
2022-04-05 00:44:07 +0100

    lint

--- train.py
+++ train.py
@@ -22,5 +22,7 @@ with open("metrics.txt", "w") as outfile:
     outfile.write("Accuracy: " + str(acc) + "\n")
 
 # Plot it
-disp = ConfusionMatrixDisplay.from_estimator(clf, X_test, y_test, normalize="true", cmap=plt.cm.Blues)
+disp = ConfusionMatrixDisplay.from_estimator(
+    clf, X_test, y_test, normalize="true", cmap=plt.cm.Blues
+)
 plt.savefig("confusion_matrix.png")
