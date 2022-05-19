62c5ae782832585262524d4dc943991729103baf
Michael Tamirie <michaeltamirie@gmail.com>
2022-05-19 22:06:38 +0300

    Edit the python

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ y_test = np.genfromtxt("data/test_labels.csv")
 
 # Fit a model
 depth = 2
-clf = RandomForestClassifier(max_depth=depth)
+clf = RandomForestClassifier(max_depth=5)
 clf.fit(X_train, y_train)
 
 acc = clf.score(X_test, y_test)
