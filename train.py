89a28aff821b379b21375fa55ef97de4fc115520
AminASDCV <99673357+AminASDCV@users.noreply.github.com>
2022-10-16 02:02:21 -0400

    Update train.py

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ X_test = np.genfromtxt("data/test_features.csv")
 y_test = np.genfromtxt("data/test_labels.csv")
 
 # Fit a model
-depth = 10
+depth = 11
 clf = RandomForestClassifier(max_depth=depth)
 clf.fit(X_train, y_train)
 
