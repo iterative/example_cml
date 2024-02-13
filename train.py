aa2a0613a91b0ff57d70661b5286d8fbc2ab6cf0
Markus Sagen <Markus.John.Sagen@gmail.com>
2024-02-13 17:00:30 +0700

    modify forest depth

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ X_test = np.genfromtxt("data/test_features.csv")
 y_test = np.genfromtxt("data/test_labels.csv")
 
 # Fit a model
-depth = 5
+depth = 2
 clf = RandomForestClassifier(max_depth=depth)
 clf.fit(X_train, y_train)
 
